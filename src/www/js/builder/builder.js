8617cac5a53637f4f18645f724d01ff6ef8a9b4f
Peter Jones <pjones@devalot.com>
2018-04-30 14:02:38 -0700

    Fix an exercise that had part of its original solution still in place

--- src/www/js/builder/builder.js
+++ src/www/js/builder/builder.js
@@ -11,11 +11,13 @@
  *
  * BONUS: (if you have time)
  *
- * Change the `Builder' function so that it takes a second argument.
- * If the second argument is given, the constructor function that is
- * generated should inherit from it.
+ * Change the `Builder' function so that it takes a second argument,
+ * another constructor function.  If the second argument is given, the
+ * constructor function that is generated should inherit from the
+ * second argument's prototype property.  This allows one to easily
+ * create a class that inherits from another class.
  */
-function Builder(properties, parent) {
+function Builder(properties) {
 
   // Your code here.
 
