1e4a7e3f2f281692782a80e7f83dffd392c19fe9
Peter Jones <pjones@devalot.com>
2018-10-02 15:15:50 -0700

    Update build system to use the latest version of Edify build helper

--- default.nix
+++ default.nix
@@ -1,68 +1,32 @@
+{
+}@args:
+
 let
   # Some helper files:
-  pinned     = import ./nix/pinned.nix;
-  nodeHelper = import ./nix/node {pkgs = pinned.pkgs;};
+  repo = fetchGit {
+    url = "git://git.devalot.com/edify.git";
+    rev = "5faef56f9d28dd8c1233d37edd7cd72af1fc03a2";
+  };
 
+  edify = import "${repo}/nix/builder.nix" args;
+  pkgs  = edify.pkgs;
+
+  nodeHelper = import ./nix/node {pkgs = edify.pkgs;};
 in
-{ pkgs ? pinned.pkgs
-}:
 
-pkgs.stdenv.mkDerivation rec {
+edify.mkDerivation rec {
   name = "webdev-${version}";
-  version = "0.7.0";
+  version = "1.0";
   src = builtins.fetchGit ./.;
 
-  # Specifically don't want fixups for this package:
-  phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+  # Extra files to include in the zip archive:
+  extraFiles = [ "LICENSE" "README.md" "start-scripts" "src" ];
 
   # Additional system dependencies:
-  buildInputs = with pkgs; [
-    inkscape      # For SVG  -> PDF
-    graphviz_2_32 # For DOT  -> PDF
-    sassc         # For SCSS -> CSS (Sass)
-
-    # Markdown -> PDF:
-    pinned.edify
-    pandoc
-    haskellPackages.pandoc-citeproc
-    haskellPackages.pandoc-crossref
-
-    # TeX (and PDF):
-    (texlive.combine {
-      inherit (texlive) scheme-small collection-binextra beamer;
-    })
-
-    # For packaging:
-    zip
-  ] ++ nodeHelper.buildInputs;
-
-  # Build things:
-  buildPhase = ''
-    edify build --top "$(pwd)" courses/*.md
-  '';
+  buildInputs = nodeHelper.buildInputs;
 
   # Install things:
-  installPhase = ''
-    dest=$out/${name}
-
-    mkdir -p $dest/handouts -p $dest/slides
-    cp -rp LICENSE README.md start-scripts src $dest/
-
-    # Copy PDF files into the correct locations:
-    find build -type f -name '*.handout.pdf' -exec cp '{}' $dest/handouts ';'
-    find build -type f -name '*.slides.pdf'  -exec cp '{}' $dest/slides ';'
-
-    # Rename PDF files:
-    for file in $dest/{handouts,slides}/*.pdf; do
-      mv $file $(echo $file | sed -E 's/[.](handout|slides)[.]pdf/.pdf/')
-    done
-
-    # Build Node.js modules and projects:
-    ${nodeHelper.installPhase}
-
-    # Build archives:
-    ( cd $out && zip -9 -y -r -q ${name}.zip ${name} )
-  '';
+  installPhase = nodeHelper.installPhase;
 
   # Shell helpers:
   shellHook = nodeHelper.shellHook;
