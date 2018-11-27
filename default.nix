a4b02a3d59e30d456ce4c3630ac36cbfd1f476fc
Peter Jones <pjones@devalot.com>
2018-11-27 14:27:47 -0700

    Bump version number

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ in
 
 edify.mkDerivation rec {
   name = "webdev-${version}";
-  version = "0.11";
+  version = "0.12";
   src = builtins.fetchGit ./.;
 
   # Extra files to include in the zip archive:
