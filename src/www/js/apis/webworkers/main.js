16ccb8ff98affbb20b35c9619f7c3703c46f1022
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-04-11 14:07:32 -0700

    Webworkers tweaks.

--- src/www/js/apis/webworkers/main.js
+++ src/www/js/apis/webworkers/main.js
@@ -1,14 +1,10 @@
-$(function() {
-  console.log("main: creating a web worker");
-  let worker = new Worker("worker.js");
+const worker = new Worker('worker.js')
 
-  console.log("main: listening for messages coming from the worker");
-  worker.addEventListener("message", function(e) {
-    console.log("main: incoming message from worker: ", e.data);
-  });
+document.querySelector('#go')
+  .addEventListener('click', () => {
+    worker.postMessage('Hello world')
+  })
 
-  document.getElementById("go").addEventListener("click", function() {
-    console.log("main: sending the worker a test value");
-    worker.postMessage("Ping");
-  });
-});
+worker.addEventListener('message', ({ data }) => {
+  console.log('Uppercased message:', data)
+})
--- src/www/js/apis/webworkers/worker.js
+++ src/www/js/apis/webworkers/worker.js
@@ -1,9 +1,6 @@
-console.log("worker: loaded");
+console.log('worker was loaded')
 
-console.log("worker: listening for messages");
-
-onmessage = function(e) {
-  console.log("worker: received message: ", e.data);
-  console.log("worker: sending response");
-  postMessage("Pong");
-};
+onmessage = ({ data }) => {
+  const capitalizedString = data.toUpperCase()
+  postMessage(capitalizedString)
+}
